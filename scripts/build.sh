6505945d1d5c7d3f7271a3a15dcbc35c44381869
annamade-design <annacheungle@gmail.com>
2026-09-20 17:37:51 +0800

    Bump version to 0.1.1

--- scripts/build.sh
+++ scripts/build.sh
@@ -14,8 +14,8 @@ cat > "$APP/Contents/Info.plist" <<'PLIST'
 <key>CFBundleIdentifier</key><string>io.github.annamade-design.LalaTranslate</string>
 <key>CFBundleName</key><string>Lala Translate</string>
 <key>CFBundlePackageType</key><string>APPL</string>
-<key>CFBundleShortVersionString</key><string>0.1.0</string>
-<key>CFBundleVersion</key><string>1</string>
+<key>CFBundleShortVersionString</key><string>0.1.1</string>
+<key>CFBundleVersion</key><string>2</string>
 <key>LSMinimumSystemVersion</key><string>15.0</string>
 <key>LSUIElement</key><true/>
 <key>NSPrincipalClass</key><string>NSApplication</string>
