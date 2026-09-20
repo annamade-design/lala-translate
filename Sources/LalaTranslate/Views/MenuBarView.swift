354f8a702370eaaec9f601fb3bb04984c099ec33
Anna Cheung <305574156+annamade-design@users.noreply.github.com>
2026-09-20 16:40:45 +0800

    Add configurable global shortcut

--- Sources/LalaTranslate/Views/MenuBarView.swift
+++ Sources/LalaTranslate/Views/MenuBarView.swift
@@ -3,7 +3,7 @@ import AppKit
 enum MenuBarView {
     static func menu(target: AppDelegate) -> NSMenu {
         let menu = NSMenu()
-        let translate = menu.addItem(withTitle: "Translate Selected Text  ⌥T", action: #selector(AppDelegate.trigger), keyEquivalent: "")
+        let translate = menu.addItem(withTitle: "Translate Selected Text  \(target.shortcut.configuration.displayName)", action: #selector(AppDelegate.trigger), keyEquivalent: "")
         translate.target = target
         let settings = menu.addItem(withTitle: "Settings…", action: #selector(AppDelegate.openSettings), keyEquivalent: ",")
         settings.target = target
